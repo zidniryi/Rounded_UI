f71e3bf244f0c164041e844c6a93d12b25813f33
zidniryi <zidnirydwan@gmail.com>
2018-10-08 08:14:09 +0700

    Initial Commit

---
+++ Rounded_UITests/Rounded_UITests.swift
@@ -0,0 +1,36 @@
+//
+//  Rounded_UITests.swift
+//  Rounded_UITests
+//
+//  Created by hint on 08/10/18.
+//  Copyright © 2018 ZidniRyi. All rights reserved.
+//
+
+import XCTest
+@testable import Rounded_UI
+
+class Rounded_UITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
